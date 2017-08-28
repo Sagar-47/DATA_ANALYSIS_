eb2aa02df1b56f62de4d582c35be78e04d9bce3e
ZEUS <kmr1997@gmail.com>
2017-08-28 15:56:39 +0530

    Third Commit

---
+++ Assignment_2/2dplots.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import csv
+
+data= pd.read_csv('facebookdata.csv')
+#get value of respective columns
+likes=data['likes'].values
+posts=data['posts'].values
+friends=data['friends'].values
+
+plt.figure(1)
+plt.hist2d(friends, posts,bins=30)
+plt.xlabel("Number of friends")
+plt.ylabel('Number of posts')
+plt.title("Distribution of friends and posts")
+plt.colorbar()
+plt.show()
+
+plt.figure(2)
+plt.hist2d(likes, posts,bins=30)
+plt.xlabel("Number of likes")
+plt.ylabel('Number of posts')
+plt.title("Distribution of likes and posts")
+plt.colorbar()
+plt.show()
+
+plt.figure(3)
+plt.hist2d(likes, friends,bins=20)
+plt.xlabel("Number of likes")
+plt.ylabel('Number of friends')
+plt.title("Distribution of likes and friends")
+plt.colorbar()
+plt.show()
